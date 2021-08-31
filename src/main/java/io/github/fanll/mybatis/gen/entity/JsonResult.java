ae2afc619b9b62dd339c5df3e30cf60487bd70b7
fanlinglong <fanlinglong@outlook.com>
2021-08-31 21:40:37 +0800

    init

---
+++ src/main/java/io/github/fanll/mybatis/gen/entity/JsonResult.java
@@ -0,0 +1,40 @@
+/**   
+ * Copyright © 2019 dream horse Info. Tech Ltd. All rights reserved.
+ * @Package: io.github.fanll.mybatis.gen.entity
+ * @author: flying-cattle  
+ * @date: 2019年4月9日 下午8:15:25 
+ */
+package io.github.fanll.mybatis.gen.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**   
+ * Copyright: Copyright (c) 2019 
+ * 
+ * <p>说明： 返回结果json对象</P>
+ * @version: v2.1.0
+ * @author: flying-cattle
+ * 
+ * Modification History:
+ * Date         	Author          Version          Description
+ *---------------------------------------------------------------*
+ * 2019年4月9日      		flying-cattle   v2.1.0           initialize
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JsonResult implements Serializable{
+	
+	private static final long serialVersionUID = 123126L;
+	
+	private Integer code;
+	
+	private String message;
+	
+	private Object data;
+}
